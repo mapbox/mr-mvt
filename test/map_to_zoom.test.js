a47d00b4241a103463d4dedaea2128e71789765c
Blake Thompson <flippmoke@gmail.com>
2016-12-15 13:41:17 -0500

    Updated tests

--- test/map_to_zoom.test.js
+++ test/map_to_zoom.test.js
@@ -42,12 +42,27 @@ test('MapToZoom - Initialization with Errors', function(t) {
     }, new RegExp('MapToZoom requires one parameter, a buffer'));
     t.end();
 });
-/*
-test('MapToZoom - Execute', function(t) {
+
+test('MapToZoom - execute - point zoom 0', function(t) {
     var m2z = new mrmvt.MapToZoom(point_buffer);
     m2z.execute(0, {}, function(err, output) {
         t.error(err);
+        t.equal(output.data, '{"type":"Point","coordinates":[2744,1613]}');
+        t.equal(output.zoom, 0);
+        t.equal(output.tiles.length, 1);
+        t.equal(output.tiles[0][0], 0);
+        t.equal(output.tiles[0][1], 0);
+        t.equal(output.tiles[0][2], false);
         t.end();
     });
 });
-*/
+
+test('MapToZoom - execute - polygon zoom 10', function(t) {
+    var m2z = new mrmvt.MapToZoom(polygon_buffer);
+    m2z.execute(10, {}, function(err, output) {
+        t.error(err);
+        t.equal(output.zoom, 10);
+        t.equal(output.tiles.length, 705);
+        t.end();
+    });
+});
